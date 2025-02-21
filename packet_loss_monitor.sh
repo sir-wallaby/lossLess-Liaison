d3d35cd0c56d5b3da48e5ba483916edbd0d393d5
Wes Taylor <wesley.taylor303@gmail.com>
2025-02-21 09:44:37 -0500

    added rtt avg/dev; updated discord message formatting;

--- packet_loss_monitor.sh
+++ packet_loss_monitor.sh
@@ -4,17 +4,18 @@ get_webhook_id() {
     grep -oP "(?<=^WEBHOOK_ID=).*" config.cfg
 }
 
-# Static portion of the webhook URL
+#static discord webhook URL
 WEBHOOK_BASE_URL="https://discord.com/api/webhooks/"
 WEBHOOK_ID=$(get_webhook_id)
 
-# Construct the full webhook URL
+#construct the full webhook URL
 WEBHOOK_URL="${WEBHOOK_BASE_URL}${WEBHOOK_ID}"
 
-#Config
+#config
 TARGET="8.8.8.8"
 THRESHOLD=0
-SLEEP_INTERVAL=30   #in seconds
+SLEEP_INTERVAL=90   #in seconds
+PACKETS_TO_SEND=100
 
 send_discord_message(){
     local message=$1
@@ -23,13 +24,21 @@ send_discord_message(){
 }
 
 while true; do
-    # Ping the target and get packet loss
-    ping_output=$(ping -c 100 $TARGET)
+    #ping the target and get latency stats
+    ping_output=$(ping -c $PACKETS_TO_SEND $TARGET)
     packet_loss=$(echo "$ping_output" | grep -oP '\d+(?=% packet loss)')
+    rtt_stats=$(echo "$ping_output" | grep 'rtt min/avg/max/mdev' | awk -F'=' '{ print $2 }' | tr -d ' ')
+    read min_rtt avg_rtt max_rtt mdev <<< $(echo "$rtt_stats" | tr '/' ' ')
 
-    # Check if packet loss is greater than the threshold
+    #discord message
+    message="**🚨 High Packet Loss Alert**"
+    message+="\n**Packet Loss($PACKETS_TO_SEND):** ${packet_loss}%\n"
+    message+="**RTT Statistics:**\n"
+    message+="\`RTT Avg:\` **${avg_rtt} ms** | \`RTT Mdev:\` **${mdev} ms**"
+
+    #check criteria and send discord message
     if (( packet_loss > THRESHOLD )); then
-        send_discord_message "🚨High Packet Loss Alert: ${packet_loss}% to $TARGET"
+        send_discord_message "$message"
     fi
     
     sleep $SLEEP_INTERVAL
